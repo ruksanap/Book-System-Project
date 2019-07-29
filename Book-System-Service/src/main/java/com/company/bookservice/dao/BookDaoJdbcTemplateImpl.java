a83b48577a77ed5be68c8c18dfc094549d41634a
Perri Love <perri.love1188@gmail.com>
2019-07-29 10:44:45 -0400

    BookDaoImpl and Testing

--- Book-System-Service/src/main/java/com/company/bookservice/dao/BookDaoJdbcTemplateImpl.java
+++ Book-System-Service/src/main/java/com/company/bookservice/dao/BookDaoJdbcTemplateImpl.java
@@ -1,33 +1,91 @@
 package com.company.bookservice.dao;
 
 import com.company.bookservice.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class BookDaoJdbcTemplateImpl implements BookDao{
 
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private static final String INSERT_BOOK_SQL =
+            "insert into book (title, author) values (?, ?)";
+
+    private static final String SELECT_BOOK_SQL =
+            "select * from book where book_id = ?";
+
+    private static final String SELECT_ALL_BOOKS_SQL =
+            "select * from book";
+
+    private static final String UPDATE_BOOK_SQL =
+            "update book set title = ?, author = ? where book_id = ?";
+
+    private static final String DELETE_BOOK_SQL =
+            "delete from book where book_id = ?";
+
     @Override
+    @Transactional
     public Book addBook(Book book) {
-        return null;
+
+        jdbcTemplate.update(INSERT_BOOK_SQL,
+                book.getTitle(),
+                book.getAuthor());
+
+        int id = jdbcTemplate.queryForObject("select last_insert_id()", Integer.class);
+        book.setBookId(id);
+
+        return book;
     }
 
     @Override
     public Book getBook(int bookId) {
-        return null;
+
+        try {
+            return jdbcTemplate.queryForObject(SELECT_BOOK_SQL, this::mapRowToBook, bookId);
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_BOOKS_SQL, this::mapRowToBook);
     }
 
     @Override
     public Book updateBook(Book book) {
-        return null;
+        jdbcTemplate.update(UPDATE_BOOK_SQL,
+                book.getTitle(),
+                book.getAuthor(),
+                book.getBookId());
+        return book;
     }
 
     @Override
     public void deleteBook(int bookId) {
+        jdbcTemplate.update(DELETE_BOOK_SQL, bookId);
 
+    }
+
+    private Book mapRowToBook (ResultSet rs, int rowNum) throws SQLException {
+        Book b = new Book();
+        b.setBookId(rs.getInt("book_id"));
+        b.setTitle(rs.getString("title"));
+        b.setAuthor(rs.getString("author"));
+
+        return b;
     }
 }
