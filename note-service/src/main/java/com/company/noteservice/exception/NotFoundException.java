ed46195ced79b067031b065c1f23d4a18a995134
unknown <prit.solanki@gmail.com>
2019-07-30 15:27:34 -0400

    Adding Controller Handler Class for note-service

---
+++ note-service/src/main/java/com/company/noteservice/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.company.noteservice.exception;
+
+//Exception class to handle not found cases in get APIs
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
